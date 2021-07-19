75e3df7835f6874bc19d25813051557fd4fcc236
Joseph Fauver <joseph.fauver@yale.edu>
2021-07-18 23:11:01 -0500

    Update apply_colour_scheme.py

--- scripts/apply_colour_scheme.py
+++ scripts/apply_colour_scheme.py
@@ -458,6 +458,17 @@ if __name__ == '__main__':
         results['category'].update({category: hex})
         print(category, hex)
     results['category'].update({'Other variants': '#808080'})
+    
+    # Cluster identification
+    list_cluster = [up_number for up_number in sorted(set(df['Cluster_ID'].to_list())) if up_number != 'None']
+    list_hex = list([hue_to_rgb(int(x)) for x in np.linspace(30, 240, len(list_cluster)*2, endpoint=True)])
+    skip_hex = [h for n, h in enumerate(list_hex) if n in range(0, len(list_hex), 2)][::-1]  
+    
+    results['Cluster_ID'] = {}
+    for Cluster_ID, hex in zip(list_cluster, skip_hex):
+        results['Cluster_ID'].update({Cluster_ID: hex})
+        print(Cluster_ID, hex)
+    results['Cluster_ID'].update({'None': '#808080'})
 
    # for us_region, hue in usregion_hues.items():
    #     start, end = hue_to_hex[hue]
